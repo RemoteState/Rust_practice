77305fa4dcac2a618bacf8e0b5fd0133080a8d05
sanskarguptaremote <125350092+sanskarguptaremote@users.noreply.github.com>
2023-02-17 13:09:00 +0530

    Update i3.rs

--- rustlings/iterators/i3.rs
+++ rustlings/iterators/i3.rs
@@ -26,33 +26,35 @@ pub fn divide(a: i32, b: i32) -> Result<i32, DivisionError> {
 // Desired output: Ok([1, 11, 1426, 3])
 fn result_with_list() -> Result<Vec<i32>, DivisionError> {
     let numbers = vec![27, 297, 38502, 81];
-    let mut result = vec![];
-    for i in numbers {
-        let b = divide(i, 27);
-        match b {
-            Ok(a) => result.push(b.unwrap()),
-            Err(e) => return Err(e),
-        }
-    }
-    Ok(result)
-    
+    // let mut result = vec![];
+    // for i in numbers {
+    //     let b = divide(i, 27);
+    //     match b {
+    //         Ok(a) => result.push(b.unwrap()),
+    //         Err(e) => return Err(e),
+    //     }
+    // }
+    // Ok(result)
+    let division_results: Result<Vec<i32>, DivisionError> = numbers.into_iter().map(|n| divide(n, 27)).collect();
+    division_results
 }
 
 // Complete the function and return a value of the correct type so the test passes.
 // Desired output: [Ok(1), Ok(11), Ok(1426), Ok(3)]
 fn list_of_results() -> Vec<Result<i32, DivisionError>> {
     let numbers = vec![27, 297, 38502, 81];
-    //let division_results: Vec<i32> = numbers.into_iter().map(|n| divide(n, 27)).collect();
+    // let mut result = vec![];
+    // for i in numbers {
+    //     let b = divide(i, 27);
+    //     match b {
+    //         Ok(a) => result.push(b),
+    //         Err(e) => result.push(Err(e)),
+    //     }
+    // }
+    // result
     
-    let mut result = vec![];
-    for i in numbers {
-        let b = divide(i, 27);
-        match b {
-            Ok(a) => result.push(b),
-            Err(e) => result.push(Err(e)),
-        }
-    }
-    result
+    let division_results: Vec<Result<i32, DivisionError>> = numbers.into_iter().map(|n| divide(n, 27)).collect();
+    division_results
 }
 
 #[cfg(test)]
